723d9074522fea6e23996053a9646c1eb39b0d32
Guan Wang <judew@fb.com>
2020-08-17 09:31:09 -0400

    async task solution

--- app/src/main/java/com/example/android100async/MainActivity.java
+++ app/src/main/java/com/example/android100async/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -14,9 +15,6 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     public static final String CAT_IMAGE = "https://www.petmd.com/sites/default/files/petmd-cat-happy-10.jpg";
 
-    // network request in main thread:
-    // java.lang.RuntimeException: Unable to start activity ComponentInfo{com.example.android100async/com.example.android100async.MainActivity}: android.os.NetworkOnMainThreadException
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,28 +22,35 @@ public class MainActivity extends AppCompatActivity {
 
         final ImageView imageView = findViewById(R.id.image_view);
 
-        new Thread() {
-            public void run() {
-                final Bitmap bmp;
-                try {
-                    bmp = downloadImage(CAT_IMAGE);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return;
-                }
+        // 3) async task
+        DownloadImageAsyncTask downloadImageAsyncTask = new DownloadImageAsyncTask();
+        downloadImageAsyncTask.execute(CAT_IMAGE);
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        imageView.setImageBitmap(bmp);
-                    }
-                });
-            }
-        }.start();
+        // 2) thread solution
+//        new Thread() {
+//            public void run() {
+//                final Bitmap bmp;
+//                try {
+//                    bmp = downloadImage(CAT_IMAGE);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    return;
+//                }
+//
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        imageView.setImageBitmap(bmp);
+//                    }
+//                });
+//            }
+//        }.start();
 
+        // 1) network request in main thread:
+        // java.lang.RuntimeException: Unable to start activity ComponentInfo{com.example.android100async/com.example.android100async.MainActivity}: android.os.NetworkOnMainThreadException
 //        try {
 //            bmp = downloadImage(CAT_IMAGE);
-//        } catch (IOException e) {
+//        } catch ( IOException e) {
 //            e.printStackTrace();
 //            return;
 //        }
@@ -56,5 +61,23 @@ public class MainActivity extends AppCompatActivity {
         URL url = new URL(s);
         InputStream is = url.openStream();
         return BitmapFactory.decodeStream(is);
+    }
+
+    class DownloadImageAsyncTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                return downloadImage(strings[0]);
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            ImageView imageView = findViewById(R.id.image_view);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
